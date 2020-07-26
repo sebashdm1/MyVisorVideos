f22c51b6f0f751dc869143615d65ad6f3073b857
sebashdm <sebashdm@gmail.com>
2020-07-26 18:10:04 -0500

    feat: se  implemntan hooks y se trae informacion de una API para darle vida a la app

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import Header from '../component/Header';
 import Search from '../component/Search';
 import Carousel from '../component/Carousel';
@@ -6,42 +6,46 @@ import Categories from '../component/Categories';
 import CarouselItem from '../component/CarouselItem';
 import Footer from '../component/Footer';
 import '../assets/styles/App.scss';
+import useInitialState from '../hooks/useInitialState.js';
+ 
+const API = 'http://localhost:3000/initialState';
 
-const App = () => (
-    <div className = "App">
-         <Header/>
-         <Search/>
-         <Categories title="Mi lista">
-             <Carousel>
-                 <CarouselItem/>
-                 <CarouselItem/>
-                 <CarouselItem/>
-                 <CarouselItem/>
-                 <CarouselItem/>
-                 <CarouselItem/>
-                 <CarouselItem/>
-             </Carousel>
-         </Categories>
-         <Categories title="Tendencias">
-             <Carousel>
-                 <CarouselItem/>
-                 <CarouselItem/>
-                 <CarouselItem/>
-                 <CarouselItem/>
-                 <CarouselItem/>
-                 <CarouselItem/>
-             </Carousel>
-         </Categories>
-         <Categories title="Productos de sebas">
-             <Carousel>
-                 <CarouselItem/>
-                 <CarouselItem/>
-                 <CarouselItem/>
-                 <CarouselItem/>
-             </Carousel>
-         </Categories>
-         <Footer/>
-    </div>
-);
+
+const App = () => {
+    
+    const initialState= useInitialState(API);
+    return initialState.length === 0 ? <h1> Loading... </h1> :(
+        <div className = "App">
+            <Header/>
+            <Search/>
+            {
+              initialState.mylist.length > 0 &&  
+              <Categories title="Mi lista">
+                <Carousel>
+                   <CarouselItem/>
+                </Carousel>
+               </Categories>
+            }
+           
+            <Categories title="Estrenos 2021">
+                <Carousel>
+                    {initialState.trends.map(item=>
+                        <CarouselItem key={item.id}{...item}/>
+                    )}  
+                </Carousel>
+            </Categories>
+
+
+            <Categories title="Productos de sebas">
+                <Carousel>
+                {initialState.originals.map(item=>
+                        <CarouselItem key={item.id}{...item}/>
+                    )}  
+                </Carousel>
+            </Categories>
+            <Footer/>
+        </div>
+  );
+}
 
 export default App;
